54628c9c2ea232c28e03fbc5f81026f0c300f26f
Sarah <50919215+scoulter10@users.noreply.github.com>
2020-08-10 11:59:10 +0100

    Update sendEmail.py

--- sendEmail.py
+++ sendEmail.py
@@ -2,12 +2,12 @@ import smtplib, ssl
 
 def send_mail():
     content="A Motion has been detected by your monitor - check your log or view your stream now"
-    recipient='sarahlouisecoulter5@gmail.com'
-    sender='sarahlouisecoulter5@gmail.com'
+    recipient='recipientemailaddress'
+    sender='senderemailaddress'
     mail=smtplib.SMTP('smtp.gmail.com', 587)
     mail.ehlo()
     mail.starttls() ##transport layer security
-    mail.login('sarahlouisecoulter5@gmail.com','gareth94')
+    mail.login('recipientemailaddress','passwordgoeshere')
     header = 'To:' + recipient + '\n' + 'From: '\
              + sender + '\n' + 'Subject:Motion Detected \n'
     content=header+content
